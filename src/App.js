3d1c6a2894d20fa9ed686720223b5524fa2a1551
sathvik <Onlinegithub123!>
2021-11-13 16:12:17 -0600

    Adding users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const addUser =  (  ) => {
+      const name = document.querySelector("#input_name").value 
+      const email = document.querySelector("#input_email").value
+
+      if (name && email) {
+        setUsers([...users, [name, email]])
+      }
+
+
+  }
+  console.log(users)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container col-6  d-flex flex-column justify-content-center"> 
+      <div>This is SANTAssign</div>
+
+        <div>
+          <h4>People: </h4>
+          {users.map(( user, idx) =>
+             <div key={idx}>
+               {user[0]} {user[1]}
+              </div>)}
+        </div>
+
+        <input id="input_name" type="text" />
+        <input id="input_email" type="email" />
+        <br/>
+        <button className='btn btn-primary' onClick={addUser}> Add </button>
+
     </div>
+
   );
 }
 
